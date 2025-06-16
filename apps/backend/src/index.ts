51edca97044140152810c2c0296c133f7e286796
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-06-16 06:53:15 -0300

    Update Vercel configuration and enhance frontend App component
    - Changed the destination in vercel.json from "/src/index.ts" to "/api/index.ts" for proper routing.
    - Refactored App component in App.tsx to export as default and updated the fetch URL to point to localhost.
    - Modified the response handling to display the fetched message as plain text.
    - Improved the layout of the message display with flexbox styling for better user experience.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,18 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: '*',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+  })
+);
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,24 +1,37 @@
 import { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
   const [message, setMessage] = useState<string>('');
 
   useEffect(() => {
     const fetchMessage = async () => {
-      const response = await fetch('/api/hello');
+      const response = await fetch('http://localhost:3000');
 
       if (!response.ok) {
         throw new Error('Failed to fetch message');
       }
 
-      const data = await response.json();
-      setMessage(data.message);
+      const data = await response.text();
+      setMessage(data);
     };
 
     fetchMessage();
   }, []);
 
-  return <h1>{message}</h1>;
-}
+  return (
+    <span
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: '1rem',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <h1>Backend Message:</h1>
 
-export default App;
+      <h1>{message}</h1>
+    </span>
+  );
+}
